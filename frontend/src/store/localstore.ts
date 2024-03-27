e94652d0c5b7e3e50f2ef220324c9a91d4c654a7
illus <illusory.racer01@mail.ru>
2024-03-28 02:33:23 +1000

    add localstore

---
+++ frontend/src/store/localstore.ts
@@ -0,0 +1,6 @@
+export const priority = ['low', 'medium', 'high'];
+export const mark = ['research', 'design', 'development' ];
+export const sort = [
+    { label: 'Новые', value: '-1' },
+    { label: 'Старые', value: '1' },
+];
