40f0a9ba63ae4380acff3d6c9c31530c10642b82
illus <illusory.racer01@mail.ru>
2024-05-03 16:15:15 +1000

    fixed logic scroll(added intesction)

--- frontend/src/components/CardList.tsx
+++ frontend/src/components/CardList.tsx
@@ -3,6 +3,7 @@ import {TCard} from "@/types/types";
 import CardItem from "@/components/CardItem";
 import {useUpdateEffect} from "@/hooks/useUpdateEffect";
 import {limit, url} from "@/store/localstore"
+import { useInView } from 'react-intersection-observer';
 
 interface CardListProps {
     cards: TCard[],
@@ -13,12 +14,13 @@ interface CardListProps {
     }
 }
 
-
 const CardList: React.FC<CardListProps> = ({cards ,filter}) => {
     const [currentPage, setCurrentPage] = useState<number>(2)
     const [cardData, setCardData] = useState<TCard[]>(cards)
     const [fetching, setFetching] = useState<boolean>(false)
     const [permissionFetching, setPermissionFetching] = useState<boolean>(true)
+    const { ref, inView } = useInView();
+
     const fetchData = async () => {
         let res = await fetch(`${url}/card?page=${currentPage}&limit=${limit}&sort=${filter.sort}&mark=${filter.mark}&priority=${filter.priority}`)
         const cardsNew: TCard[] = await res.json()
@@ -30,17 +32,17 @@ const CardList: React.FC<CardListProps> = ({cards ,filter}) => {
         setFetching(false)
     }
 
-    const scrollHandler = (e: any) => {
-        if ((e.target.documentElement.scrollHeight - (e.target.documentElement.scrollTop + window.innerHeight) < e.target.documentElement.scrollHeight / 100 * 15) && permissionFetching) {
-            setFetching(true)
-        }
-    }
-
     useEffect( () => {
         if(fetching) {
             fetchData()
         }
     },[fetching])
+
+    useUpdateEffect( () => {
+        if(inView && permissionFetching) {
+            setFetching(true)
+        }
+    }, [inView])
 
     useUpdateEffect(() => {
         setCurrentPage(1)
@@ -48,13 +50,6 @@ const CardList: React.FC<CardListProps> = ({cards ,filter}) => {
         setPermissionFetching(true)
         setFetching(true)
     }, [filter])
-
-    useEffect(() => {
-        document.addEventListener('scroll', scrollHandler)
-        return function () {
-            document.removeEventListener('scroll', scrollHandler)
-        }
-    }, [permissionFetching])
 
     return (
         <div className={"container-main"}>
@@ -66,11 +61,15 @@ const CardList: React.FC<CardListProps> = ({cards ,filter}) => {
                         {cardData.map((card: any) => (
                             <CardItem key={card._id} card={card}/>
                         ))}
+                        {permissionFetching ?
+                        <div className={"container-loader"}>
+                            <div className={"loader"}ref={ref}></div>
+                        </div> : null
+                        }
+                        
                     </div>
-
                 }
             </>
-
         </div>
     );
 };
